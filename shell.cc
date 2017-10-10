2d4c0ff0133ce10bc034c62c09d9b349806e5e39
Jackie Wu <jackielwu@gmail.com>
2017-10-10 13:09:35 -0400

    Commit

--- shell.cc
+++ shell.cc
@@ -27,7 +27,7 @@ int main() {
   struct sigaction sa1;
   sa1.sa_handler = controlC;
   sigemptyset(&sa1.sa_mask);
-  sa1.sa_falgs = SA_RESTART;
+  sa1.sa_flags = SA_RESTART;
   error = sigaction(SIGINT, &sa1, NULL);
   if (error) {
     perror("sigaction");
