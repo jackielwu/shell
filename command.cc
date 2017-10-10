ac379f6b916e39640572eb5c24be9d5bb666cb65
Jackie Wu <jackielwu@gmail.com>
2017-10-10 14:16:09 -0400

    Commit

--- command.cc
+++ command.cc
@@ -110,7 +110,7 @@ void Command::print() {
 int Command::builtInCheck(int i) {
   //Check if builtin command is called
   if (!strcmp(_simpleCommands[i]->_arguments[0], "printenv")) {
-    int error = printenv();
+    int error = printf("%s\n",environ);
     if (error)
       perror("printenv");
     clear();
