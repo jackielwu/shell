9304cbb5edf3940fee100165f0b18ef15f25c216
Jackie Wu <jackielwu@gmail.com>
2017-10-10 13:07:19 -0400

    Commit

--- shell.cc
+++ shell.cc
@@ -1,7 +1,39 @@
 #include "command.hh"
 int yyparse(void);
 
+extern "C" void controlC(int sig) {
+  printf("\n");
+  Command::_currentCommand.prompt();
+}
+
+
+
+
+
+
+
+
+
 int main() {
+
+  int error;
+
+  //Control-c
+  struct sigaction sa1;
+  sa1.sa_handler = controlC;
+  sigemptyset(&sa1.sa_mask);
+  sa1.sa_falgs = SA_RESTART;
+  error = sigaction(SIGINT, &sa1, NULL);
+  if (error) {
+    perror("sigaction");
+    exit(-1);
+  }
+
+
+
+
+
+
 	Command::_currentCommand.prompt();
 	yyparse();
 }
