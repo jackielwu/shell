9d3210e45ddfc9f03b6d0c55461b51f376fb0730
Jackie Wu <jackielwu@gmail.com>
2017-10-10 12:58:19 -0400

    Commit

--- command.cc
+++ command.cc
@@ -235,33 +235,4 @@ void Command::prompt() {
 
 Command Command::_currentCommand;
 SimpleCommand * Command::_currentSimpleCommand;
-
-int yyparse(void);
-
-
-extern "C" void controlC(int sig) {
-  printf("\n");
-  Command::_currentCommand.prompt();
-}
-
-
-
-main() {
-  
-  int error;
-
-  //Control-c
-  struct sigaction sa1;
-  sa1.sa_handler = controlC;
-  sigemptyset(&sa1.sa_mask);
-  sa1.sa_flags = SA_RESTART;
-  error = sigaction(SIGINT, &sa1, NULL);
-  if (error) {
-    perror("sigaction");
-    exit(-1);
-  }
-
-  Command::_currentCommand.prompt();
-  yyparse();
-
 }
